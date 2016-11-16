fe2c54939c5417c25403a4b9c6a308a400e5b604
Gideon Lee <Gideonxlee@gmail.com>
2016-11-16 15:42:50 -0800

    Added create and index operations

--- server/controllers/pets.js
+++ server/controllers/pets.js
@@ -1,7 +1,22 @@
 var Pet = require('./../models/pet');
 
 module.exports = {
+  index: function(req, res) {
+  	Pet.find({}, function(err, allPets) {
+  		if (err) {
+  			res.json(err);
+  		} else {
+			res.json(allPets);
+  		}
+  	})
+  },
   create: function(req, res){
-    console.log(req.body);
+  	Pet.create(req.body, function(err) {
+  		if (err) {
+  			res.json(err);
+  		} else {
+  			res.redirect('/');
+  		}
+  	});
   }
 };
